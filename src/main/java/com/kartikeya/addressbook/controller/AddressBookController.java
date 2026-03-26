e241ee9ebf4ced2773ccaa901024a3b67e15b7f0
Kartikeya Pusapati <swagboss600@gmail.com>
2026-03-26 15:09:25 +0530

    [Kartikeya]: UC2 CRUD APIs implemented

--- src/main/java/com/kartikeya/addressbook/controller/AddressBookController.java
+++ src/main/java/com/kartikeya/addressbook/controller/AddressBookController.java
@@ -1,17 +1,42 @@
 package com.kartikeya.addressbook.controller;
 
 /*
- * UC1: Basic AddressBook Controller
+ * UC2: CRUD REST Controller
  */
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/addressbook")
 public class AddressBookController {
 
-    // GET all
+    // GET
     @GetMapping
-    public String getAll() {
-        return "All contacts";
+    public ResponseEntity<String> getAll() {
+        return ResponseEntity.ok("Get all contacts");
+    }
+
+    // GET BY ID
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getById(@PathVariable int id) {
+        return ResponseEntity.ok("Get contact " + id);
+    }
+
+    // POST
+    @PostMapping
+    public ResponseEntity<String> create() {
+        return ResponseEntity.ok("Created");
+    }
+
+    // PUT
+    @PutMapping("/{id}")
+    public ResponseEntity<String> update(@PathVariable int id) {
+        return ResponseEntity.ok("Updated " + id);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable int id) {
+        return ResponseEntity.ok("Deleted " + id);
     }
 }
