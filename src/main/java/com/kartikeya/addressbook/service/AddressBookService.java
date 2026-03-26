3ba41ddd91a3c8d9c957267e0a458190a84eff1e
Kartikeya Pusapati <swagboss600@gmail.com>
2026-03-26 15:09:31 +0530

    [Kartikeya]: UC4 service layer implemented

---
+++ src/main/java/com/kartikeya/addressbook/service/AddressBookService.java
@@ -0,0 +1,15 @@
+package com.kartikeya.addressbook.service;
+
+/*
+ * Service Layer
+ */
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressBookService {
+
+    // sample method
+    public String getData() {
+        return "Service layer working";
+    }
+}
